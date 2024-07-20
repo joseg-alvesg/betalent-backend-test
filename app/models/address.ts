0d77560bafa01f2620d133417b105116304a66bd
joseg-alvesg <cinhoalves85@gmail.com>
2024-07-20 18:43:56 -0300

    address model/migration

---
+++ app/models/address.ts
@@ -0,0 +1,39 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Client from '#models/client'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export default class Address extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => Client)
+  declare client: BelongsTo<typeof Client>
+
+  @column()
+  declare street: string
+
+  @column()
+  declare number: string
+
+  @column()
+  declare complement: string
+
+  @column()
+  declare neighborhood: string
+
+  @column()
+  declare city: string
+
+  @column()
+  declare state: string
+
+  @column()
+  declare zipCode: string
+}
---
+++ database/migrations/1721493466076_create_addresses_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'addresses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('client_id').unsigned().references('id').inTable('clients').onDelete('CASCADE')
+      table.string('street').notNullable()
+      table.string('number').notNullable()
+      table.string('complement').nullable()
+      table.string('neighborhood').notNullable()
+      table.string('city').notNullable()
+      table.string('state').notNullable()
+      table.string('zip_code').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
